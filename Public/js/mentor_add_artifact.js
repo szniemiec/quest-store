d74e5c8e471661f368210904f4c08445d35a64b6
KozubIgn <ignacykozub@gmail.com>
2020-07-08 22:12:40 +0200

    add artifacts display

---
+++ Public/js/mentor_add_artifact.js
@@ -0,0 +1,28 @@
+
+const formArtifact = document.querySelector("#add_artifact")
+
+// wysyłamy wpisane dane w login i password po kliknieciu klawisza login - flaga "submit"
+formArtifact.addEventListener('submit', function (e) {
+    // zapobiega dwuktronemu wysylaniu formularza
+    e.preventDefault();
+
+    const data = `name=${this.title.value}&description=${this.description.value}&reward=${this.cost.value}`;
+
+    setArtifact(data);
+});
+
+function setArtifact(data) {
+    fetch("http://localhost:8000/add-artifact",
+        {
+            mode: 'no-cors',
+            method: "POST",
+            body: data
+        })
+        .then(function (response) {
+
+        })
+}
+function showMessage() {
+    document.querySelector(".message").classList.toggle("hide");
+    return false;
+}
---
+++ Public/js/mentor_artifacts.js
@@ -0,0 +1,30 @@
+const container = document.querySelector(".cards")
+
+function getArtifacts() {
+    fetch('http://localhost:8000/artifacts')
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(function (artifacts) {
+            innerArtifacts(artifacts);
+        })
+}
+
+function innerArtifacts(artifacts) {
+    let cards = document.querySelector(".cards")
+    artifacts.forEach(artifact => {
+
+        let card = document.createElement("div");
+        card.setAttribute('class', 'card');
+        card.innerHTML =
+            `<div class="card-title">${artifact.name}</div>` +
+            `<div class="card-description">${artifact.description}</div>` +
+            `<div class="card-pillPrice">Price : ${artifact.cost}</div>`;
+
+        cards.appendChild(card);
+    });
+}
+
+getArtifacts();
