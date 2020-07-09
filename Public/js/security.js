3c96794458d24f27e590fdd3c4e18bb5ccc14743
KozubIgn <ignacykozub@gmail.com>
2020-07-09 23:16:35 +0200

    add delete sessionStorage

--- Public/js/security.js
+++ Public/js/security.js
@@ -1,3 +1,12 @@
+
+/// tymczasowe rozwiazanie. Strosuje sie autoryzacje JWTToken
 if( !sessionStorage.getItem('id')) {
+    window.location.href="login.html";
+}
+//clear sessionStorage
+const  logoutBtn = document.querySelector('.logout');
+logoutBtn.addEventListener('click', logout);
+function logout() {
+    sessionStorage.clear();
     window.location.href="login.html";
 }
